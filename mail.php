d0a1f8b5405411a8a9cc96926492f7e900435167
thatguy <xarodasimba@gmail.com>
2019-11-12 17:01:58 +0200

    initial commit

---
+++ mail.php
@@ -0,0 +1,16 @@
+<?php
+if(isset( $_POST['fname']))
+$fname = $_POST['fname'];
+if(isset( $_POST['email']))
+$email = $_POST['email'];
+if(isset( $_POST['message']))
+$message = $_POST['message'];
+if(isset( $_POST['company']))
+$company = $_POST['company'];
+
+$content="From: $fname \n Email: $email \n Message: $message";
+$recipient = "xarodasimba@gmail.com";
+$mailheader = "From: $email \r\n";
+mail($recipient, $company, $content, $mailheader) or die("Error!");
+echo "Email sent!";
+?>
